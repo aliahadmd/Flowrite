625c32e757c9b1cc2cb1f080bfb0486dc9cd7e46
Ali <aliahadmd1@gmail.com>
2024-03-16 00:43:46 +0600

    improve performance

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-import express from 'express';
-import sequelize from './config/data.config.js';
-import homeRoute from './routes/home.route.js';
-import adminRoute from './routes/admin.route.js';
-import session from 'express-session';
-import expressLayouts from 'express-ejs-layouts';
+import "dotenv/config";
+import express from "express";
+import sequelize from "./config/data.config.js";
+import homeRoute from "./routes/home.route.js";
+import adminRoute from "./routes/admin.route.js";
+import session from "express-session";
+import expressLayouts from "express-ejs-layouts";
 
 const app = express();
 
 //middleware
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // Session middleware
 app.use(
   session({
-    secret: 'secret-key',
+    secret: "secret-key",
     resave: false,
     saveUninitialized: false,
   })
 );
 
-
 //ejs setup
-app.use(expressLayouts)
-app.set('layout','layout/layout')
-app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set("layout", "layout/layout");
+app.set("view engine", "ejs");
 // static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/', homeRoute);
-app.use('/admin', adminRoute);
-
+app.use("/", homeRoute);
+app.use("/admin", adminRoute);
 
 try {
-    // database configuration
-    sequelize.sync()
-    console.log('Database connection has been established successfully.');
-    app.listen(8090, () => console.log('Server running on port 4001'));
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  // database configuration
+  sequelize.sync();
+  console.log("Database connection has been established successfully.");
+  app.listen(8090, () => console.log("Server running on port 8090"));
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
